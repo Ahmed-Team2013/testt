ab71d05c408ed86553a6982335aa06e50a841a48
Ahmed-Team2013 <ahmed.team2013@gmail.com>
2025-09-21 23:30:47 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+let users = [
+  {username:"وائل", password:"123", role:"sales"},
+  {username:"admin", password:"admin", role:"manager"}
+];
+let projects = [];
+function handleLogin(){ alert('Login clicked'); }
